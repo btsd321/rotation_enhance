a0979c2ff6d8ed3b1530b9f9a57cdccc6cefb738
btsd321 <1123347532@qq.com>
2025-06-26 12:22:19 +0800

    feat(example): 添加图片集、标签集、输出路径、旋转角度列表和预训练模型路径的注释
    - 添加了对 `test_imgs_folder`、`test_labels_folder`、`test_output_folder` 的注释，描述了这些变量分别存储原始图片集路径、原始标签集路径和输出路径。
    - 添加了对 `test_angles_list` 的注释，说明了该变量是一个旋转角度列表，可以包含正数和负数，并且以角度制表示。
    - 添加了对 `test_model_path` 的注释，指出了该变量存储预训练模型路径，并特别说明了目前只支持 `.pt` 格式的模型。
    - 注释的添加有助于提高代码的可读性和可维护性。

--- example/example.py
+++ example/example.py
@@ -1,10 +1,17 @@
 from rotation_enhance import rotation_enhance
 
+# 原始图片集路径
 test_imgs_folder = "D:/Project/all_data_5_r_rune_fitered/rotation_enhance/test/images"
+# 原始标签集路径
 test_labels_folder = "D:/Project/all_data_5_r_rune_fitered/rotation_enhance/test/labels"
+# 输出路径
 test_output_folder = "D:/Project/all_data_5_r_rune_fitered/rotation_enhance/test/output"
+# 旋转角度列表，可正可负，角度制
 test_angles_list = [10, 20, 30]
+# 预训练模型路径，暂时只支持pt格式
 test_model_path = "D:/Project/all_data_5_r_rune_fitered/rotation_enhance/test/model/best.pt"
+
+# 创建并运行 RotationEnhance 实例
 woker = rotation_enhance.RotationEnhance(imgs_folder = test_imgs_folder, 
                         labels_folder = test_labels_folder, 
                         output_folder = test_output_folder, 
